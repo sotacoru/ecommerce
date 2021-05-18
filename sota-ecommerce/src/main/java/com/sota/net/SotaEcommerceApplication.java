76b5984c97fe88b59a6ce75888c08effba710bb7
Adrianmc <adri.miraz@gmail.com>
2021-05-18 18:07:19 +0200

    Creacion back proyecto ecommerce

---
+++ sota-ecommerce/src/main/java/com/sota/net/SotaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sota.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SotaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SotaEcommerceApplication.class, args);
+	}
+
+}
---
+++ sota-ecommerce/src/test/java/com/sota/net/SotaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sota.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SotaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
