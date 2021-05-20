82615379803362384745ed1866722eb8bc944e30
carlos <carlosjt@gmail.com>
2021-05-20 18:01:16 +0200

    New inserts

---
+++ sota-ecommerce/src/main/resources/import.sql
@@ -0,0 +1,32 @@
+
+
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Café Jurado molido ecológico 250 gr.", 3,15, "El café molido ecológico Café Jurado es un café molido 100% arábica de agricultura ecológica.", 15, null, 5);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Café Chiapas Bio Alternativa, 250g ", 4,64, "Cultivado por grupos campesinos en las montañas de Chiapas, sin agroquímicos, siguiendo técnicas tradicionales respetuosas con la Naturaleza", 5, null, 5);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Café en Grano selección 100% Arábica Bio Destination",11,64, "Café en Grano selección 100% Arábica, mezcla de cafés arábicos orgánicos puros de América del Sur y Central cultivados en las laderas de los Andes", 20, null, 5);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Café Biológico en grano Alternativa ", 5,10, "Café en grano selecto de las variedades más apreciadas de Arábica centroamericanos y robustas africanos.", 5, null, 5);
+
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "12x Huevos ecológicos Granja Redondo", 3,85, "Producidos por gallinas que viven al aire libre con un espacio de 8 m2 por cada gallina, se alimentan con semillas ecológicas, de ahí la calidad del producto.", 20, null, 4);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "20x Huevos de Oro XL ecológicos Granja Redondo", 6,90, "20 huevos frescos de Gallina criada y alimentada en sistema ecológico (código 0), envasados en cartón de 20 alveolos apto para uso alimentario.", 8, null, 4);
+
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Pack PROMO 6 uds Leche Entera ecológica Puleva, 1 L", 8,15, "Proviene de granjas seleccionadas, donde las vacas reciben una alimentación sana y natural procedente de agricultura ecológica, sin herbicidas ni pesticidas, y gozan de libertad para pastar en prados abiertos y se cuida de su salud y bienestar.", 10, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Pack 2x Leche semidesnatada de vaca BIO Cantero de Letur 1 l", 6,71, "Pack 2x Leche semidesnatada de vaca BIO Cantero de Letur, 1 l", 15, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Leche de Cabra UHT Andechser, 1L ", 4,70, "Leche de Cabra UHT con 3,0% de Grasa.", 5, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Mantequilla Ghee Bio keto Drasanvi 300 g", 9,72, "La mantequilla ghee de Drasanvi es ecológica por lo que en su proceso de elaboración no se han utilizado sustancias químicas, consiguiendo un producto más natural y saludable.", 25, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "MeGhee Bio cabra Golden Ghee 300 g", 18,80, "MeGhee es un producto ayurveda orgánico utilizado para usos culinarios y/o terapéuticos.", 15, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Ghee Mantequilla Clarificada 100% natural Vegetalia ", 8,90, "Ghe Mantequilla Clarificada 100% Natural Vegetalia. Elaborado con leche de vaca.", 12, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Queso vegano Vegancheese ahumado Grano vita 200 g", 2,88, "Delicioso 'queso vegano' de anacardo, ideal para tomar en crudo como para cocinar, con suave y cremosa textura.", 4, null, 8);
+INSERT INTO producto (`nombre`, `precio`, `descrpcion`, `cantidad`, `foto`, `idCategoria`) 
+VALUES ( "Queso vegano Veggieques lonchas BIO Natursoy, 150 g", 2,72, "Ingredientes: Agua, grasa de coco*(20%), almidón de patata*, harina de ALTRAMUZ*, sal marina, espesantes: goma xantana y agar-agar*, extracto de cúrcuma*, aromas naturales.", 10, null, 8);
