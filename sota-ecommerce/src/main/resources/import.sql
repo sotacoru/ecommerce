018550c0573d550e567d8984c6bcf3bba210a4ab
David <davidferlop0@gmail.com>
2021-05-20 17:12:29 +0200

    Añadir categorias y bebidas alcoholicas

---
+++ sota-ecommerce/src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Bebidas alcoholicas','Bebidas que contengan un porcentaje de alcohol mayor a 0');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Frutas','Frutas');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Verduras','Verduras que no contienen químicos ni son alteradas genéticamente');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Huevos','Huevos de granjas ecologicamente sostenibles');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Cafe e infusiones','');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Postres','Postres realizados con productos naturales y sin conservantes');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Cereales','');
+INSERT INTO categoria (nombreCategoria,descripcion) VALUES ('Lacteos','Productos lácteos sin aditivos ni conservantes');
+
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Estrella Galicia',1.20,'Cerveza Estrella Galicia con 4,5% vol.',40,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Vino Château Cheval Blanc',25000,'Vino pres',5,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Whisky Red Label',20,'Whisky de la marca Johnnie Walker',34,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Licor 43',23,'Contiene 27% vol.',20,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Absolut Vodka ',25,'Vodka de la marca Absolut. Contiene 35% vol.',15,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Ron Barcelò',14,'Ron de la marca Ron Barcelò',65,null,1);
+INSERT INTO producto (nombre,precio,descripcion,cantidad,foto,idCategoria) VALUES ('Ginebra Larios',12,'Ginebra Larios sabor a fresa. Contiene 12% vol.',36,null,1);
