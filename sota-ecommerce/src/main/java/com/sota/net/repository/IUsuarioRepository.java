7d34bdb4870abd2281828ac1543bdb8b1e9be177
David <davidferlop0@gmail.com>
2021-05-21 13:05:31 +0200

    Implementando Repository Usuarios

---
+++ sota-ecommerce/src/main/java/com/sota/net/repository/IUsuarioRepository.java
@@ -0,0 +1,23 @@
+package com.sota.net.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sota.net.entity.Usuario;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	@Query("select u from Usuario u where u.nombre like %?1%")
+	public List<Usuario> findByNombre(Usuario nombre);
+
+	@Query("from Usuario")
+	public List<Usuario> findAllUsuarios();
+	
+	@Query("select u from Usuario u where u.id = ?1")
+	public void deleteById(Long idUsuario);
+	
+	@Query("select u from Usuario u where u.id = ?1")
+	public Usuario updateById(Usuario idUsuario);
+}
