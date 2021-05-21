f5b00b9ad25b3b27bff364ce09e07a3deeb79aee
Adrianmc <adri.miraz@gmail.com>
2021-05-21 12:32:57 +0200

    Añadido Serializable de las clases Perfil y Pago

--- sota-ecommerce/src/main/java/com/sota/net/entity/Pago.java
+++ sota-ecommerce/src/main/java/com/sota/net/entity/Pago.java
@@ -1,5 +1,7 @@
 package com.sota.net.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "Pago")
-public class Pago {
+public class Pago implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- sota-ecommerce/src/main/java/com/sota/net/entity/Perfil.java
+++ sota-ecommerce/src/main/java/com/sota/net/entity/Perfil.java
@@ -1,5 +1,7 @@
 package com.sota.net.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "Perfil")
-public class Perfil {
+public class Perfil implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
