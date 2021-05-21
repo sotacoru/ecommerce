0ffb72eb36baa4f0fbe24762a3c693f70589a6e4
David <davidferlop0@gmail.com>
2021-05-21 10:51:21 +0200

    Implementando entidad usuario

--- sota-ecommerce/src/main/java/com/sota/net/entity/Usuario.java
+++ sota-ecommerce/src/main/java/com/sota/net/entity/Usuario.java
@@ -1,5 +1,111 @@
 package com.sota.net.entity;
 
-public class Usuario {
+import java.io.Serializable;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idUsuario;
+
+	private String nombre;
+
+	private String primerApellido;
+
+	private String segundoApellido;
+
+	@Column(unique = true)
+	private String email;
+
+	private String contraseña;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name="perfil")
+	@JoinColumn(name="idPerfil")
+	private Long idPerfil;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name="pago")
+	@JoinColumn(name="idPago")
+	private Long idPago;
+
+	
+	// Metodos getter y setter
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public Long getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Long idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+	public Long getIdPago() {
+		return idPago;
+	}
+
+	public void setIdPago(Long idPago) {
+		this.idPago = idPago;
+	}
+
+	private static final long serialVersionUID = 1L;
 }
