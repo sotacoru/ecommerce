76ba4bab5d93763a876ab408a3ad25dcbdbfe6f4
Adrianmc <adri.miraz@gmail.com>
2021-05-21 10:58:05 +0200

    Creadas Entity pago y perfil

--- sota-ecommerce/src/main/java/com/sota/net/entity/Perfil.java
+++ sota-ecommerce/src/main/java/com/sota/net/entity/Perfil.java
@@ -1,5 +1,52 @@
 package com.sota.net.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Perfil")
 public class Perfil {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPerfil")
+	private Long idPerfil;
+	
+	
+	@Column( name = "nombrePerfil")
+	private String nombrePerfil;
+	
+	
+	@Column( name = "descripcion")
+	private String descripcion;
+
+
+	public Long getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Long idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+	public String getNombrePerfil() {
+		return nombrePerfil;
+	}
+
+	public void setNombrePerfil(String nombrePerfil) {
+		this.nombrePerfil = nombrePerfil;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 }
