b3ea979e5be693fd032409f2441903b918b61a44
carlos <carlosjt@gmail.com>
2021-05-21 11:32:34 +0200

    Producto repo

--- sota-ecommerce/src/main/java/com/sota/net/repository/IProductoRepository.java
+++ sota-ecommerce/src/main/java/com/sota/net/repository/IProductoRepository.java
@@ -8,32 +8,28 @@ import org.springframework.data.repository.CrudRepository;
 import com.sota.net.entity.Categoria;
 import com.sota.net.entity.Producto;
 
-public interface IProductoRepository extends CrudRepository<Producto, Long>{
-	
-	
-	
+public interface IProductoRepository extends CrudRepository<Producto, Long> {
+
 	@Query("select p from producto p where  p.nombre like %?1%")
 	public List<Producto> findByNombre(String nombre);
-	
+
 	@Query("select p from producto p where  p.descripcion like %?1%")
 	public List<Producto> findByDescripcion(String term);
-	
-	//public List<Producto> findByCategoria(int categoriaProducto);
-	
+
+	// public List<Producto> findByCategoria(int categoriaProducto);
+
 	@Query("select p from producto p where  p.cantidad > 0")
-	public List<Producto> findByStock(int cantidad );
-	
-	
+	public List<Producto> findByStock(int cantidad);
+
 	@Query("select p from producto p ORDER BY p.nombre")
 	public List<Producto> OrderByNombre(String nombre);
-	
-	
+
 	@Query("select p from producto p ORDER BY p.precio ASC")
 	public List<Producto> OrderByPricioMax(Double precio);
-	
+
 	@Query("select p from producto p ORDER BY p.precio DESC")
 	public List<Producto> OrderByPricioMin(Double precio);
-	
+
 	@Query("from categoria")
 	public List<Categoria> findAllCategoria();
 
