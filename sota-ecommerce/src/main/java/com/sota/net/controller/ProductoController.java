7192f9409fda77c80c03ef4bfc00920d7460436e
carlos <carlosjt@gmail.com>
2021-05-24 12:51:41 +0200

    Put producto creado

--- sota-ecommerce/src/main/java/com/sota/net/controller/ProductoController.java
+++ sota-ecommerce/src/main/java/com/sota/net/controller/ProductoController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +33,29 @@ public class ProductoController {
 		
 	}
 	
-	@GetMapping("/clientes")
+	@PutMapping("/producto/{id}")
+	public ResponseEntity<?> update( @RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoActual = productoService.findById(id);
+		Producto productoUpdate = null;
+		
+		if (productoActual == null) {
+		
+		}
+		productoActual.setNombre(producto.getNombre());
+		productoActual.setCantidad(producto.getCantidad());
+		productoActual.setDescripcion(producto.getDescripcion());
+		productoActual.setIdcategoria(producto.getIdcategoria());
+		productoActual.setPrecio(producto.getPrecio());
+		productoUpdate = productoService.save(productoActual);
+		
+		return new ResponseEntity<>(productoUpdate, HttpStatus.CREATED);
+		
+		
+	}
+	
+	
+	
+	@GetMapping("/productos")
 	public List<Producto> index() {
 		return productoService.findAll();
 	}
