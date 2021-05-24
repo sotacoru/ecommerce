953cd38ff09b447b636777a6d392bb5a6bc28bbc
Adrianmc <adri.miraz@gmail.com>
2021-05-24 17:00:46 +0200

    Creada clase SecurityConfig

---
+++ sota-ecommerce/src/main/java/com/sota/net/configuration/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.sota.net.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().anyRequest();
+	}
+	
+}
--- sota-ecommerce/src/main/java/com/sota/net/controller/UsuarioController.java
+++ sota-ecommerce/src/main/java/com/sota/net/controller/UsuarioController.java
@@ -5,9 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
