fa0875a3f3d81ed594150be2137825a632d06a6d
Adrianmc <adri.miraz@gmail.com>
2021-05-24 12:05:51 +0200

    Hola

--- sota-ecommerce/src/main/java/com/sota/net/entity/Usuario.java
+++ sota-ecommerce/src/main/java/com/sota/net/entity/Usuario.java
@@ -2,7 +2,6 @@ package com.sota.net.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
