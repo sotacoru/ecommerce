93a3feda89afd4950da9331095bb106764d22b97
David <davidferlop0@gmail.com>
2021-05-25 12:49:13 +0200

    Creando componentes, insertando scss

---
+++ front-eccommerce/src/app/detalle-productos/detalle-productos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetalleProductosComponent } from './detalle-productos.component';
+
+describe('DetalleProductosComponent', () => {
+  let component: DetalleProductosComponent;
+  let fixture: ComponentFixture<DetalleProductosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetalleProductosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetalleProductosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-eccommerce/src/app/form-login-registro/form-login-registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-login-registro',
+  templateUrl: './form-login-registro.component.html',
+  styleUrls: ['./form-login-registro.component.scss']
+})
+export class FormLoginRegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
