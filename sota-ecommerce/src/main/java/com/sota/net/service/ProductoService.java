f5aea2e7ffede3dcfec014740268bb20e6105ca4
Adrianmc <adri.miraz@gmail.com>
2021-05-26 15:06:51 +0200

    version final jWT

--- sota-ecommerce/src/main/java/com/sota/net/service/ProductoService.java
+++ sota-ecommerce/src/main/java/com/sota/net/service/ProductoService.java
@@ -1,7 +1,9 @@
 package com.sota.net.service;
 
 import com.sota.net.entity.Categoria;
+
 import com.sota.net.entity.Producto;
+import com.sota.net.entity.Producto_;
 import com.sota.net.entity.dto.ProductoBusqueda;
 import com.sota.net.repository.*;
 import com.sota.net.utils.busqueda.ProductoCriteria;
@@ -64,14 +66,14 @@ public class ProductoService extends QueryService implements IProductoService {
             return specification;
         }
         if (criteria.getNombre() != null) {
-         //  specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
+         specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
         }
         if (criteria.getDescripcion() != null) {
-            //specification =
-                   // specification.and(this.buildStringSpecification(criteria.getDescripcion(), Producto_.descripcion));
+            specification =
+                   specification.and(this.buildStringSpecification(criteria.getDescripcion(), Producto_.descripcion));
         }
         if (criteria.getHaveFoto() != null) {
-           // specification = specification.and(this.buildSpecification(criteria.getHaveFoto(), Producto_.foto));
+            specification = specification.and(this.buildSpecification(criteria.getHaveFoto(), Producto_.foto));
         }
 
 
