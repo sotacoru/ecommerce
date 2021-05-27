0f6dece914ed5276ee8c515bb5c6df9ab668a731
Adrianmc <adri.miraz@gmail.com>
2021-05-27 11:54:26 +0200

    Errores solucionados

--- sota-ecommerce/src/main/java/com/sota/net/service/impl/ProductoService.java
+++ sota-ecommerce/src/main/java/com/sota/net/service/impl/ProductoService.java
@@ -2,8 +2,9 @@ package com.sota.net.service.impl;
 
 import com.sota.net.entity.Categoria;
 
+
 import com.sota.net.entity.Producto;
-import com.sota.net.entity.Producto_;
+//import com.sota.net.entity.Producto_;
 import com.sota.net.entity.dto.ProductoBusqueda;
 import com.sota.net.repository.IProductoRepository;
 import com.sota.net.service.IProductoService;
@@ -70,14 +71,14 @@ public class ProductoService extends QueryService implements IProductoService {
             return specification;
         }
         if (criteria.getNombre() != null) {
-           specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
+          // specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
         }
         if (criteria.getDescripcion() != null) {
-            specification =
-                 specification.and(this.buildStringSpecification(criteria.getDescripcion(), Producto_.descripcion));
+            //specification =
+              //   specification.and(this.buildStringSpecification(criteria.getDescripcion(), Producto_.descripcion));
         }
         if (criteria.getHaveFoto() != null) {
-          specification = specification.and(this.buildSpecification(criteria.getHaveFoto(), Producto_.foto));
+          //specification = specification.and(this.buildSpecification(criteria.getHaveFoto(), Producto_.foto));
         }
 
 
