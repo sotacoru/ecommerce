810e20d59a452a16560b82fef2bb5b5879714230
Antía Castro <83276447+antiacastro95@users.noreply.github.com>
2021-05-31 12:56:40 +0200

    Update ProductoService.java
    Borrar comentarios de mas

--- sota-ecommerce/src/main/java/com/sota/net/service/impl/ProductoService.java
+++ sota-ecommerce/src/main/java/com/sota/net/service/impl/ProductoService.java
@@ -2,10 +2,6 @@ package com.sota.net.service.impl;
 
 import com.sota.net.entity.Categoria;
 import com.sota.net.entity.Producto;
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
 import com.sota.net.entity.Producto_;
 import com.sota.net.entity.dto.ProductoBusqueda;
 import com.sota.net.repository.IProductoRepository;
@@ -81,11 +77,8 @@ public class ProductoService extends QueryService implements IProductoService {
             return specification;
         }
         if (criteria.getNombre() != null) {
-<<<<<<< HEAD
         specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
-=======
-          specification = specification.and(this.buildStringSpecification(criteria.getNombre(), Producto_.nombre));
->>>>>>> develop
+
         }
         if (criteria.getDescripcion() != null) {
             specification =
