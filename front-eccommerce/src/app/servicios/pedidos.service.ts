d307709e140a4bb2fd758ce30286da1e857333fc
antia.castro <antiacasga@gmail.com>
2021-06-02 08:34:52 +0200

    Servicio terminado en front

--- front-eccommerce/src/app/servicios/pedidos.service.ts
+++ front-eccommerce/src/app/servicios/pedidos.service.ts
@@ -1,20 +1,34 @@
-import { Injectable } from '@angular/core';
-import {Observable, throwError} from "rxjs";
-import {catchError} from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Pedido} from "../pedidos/pedido";
-import {ActivatedRoute} from "@angular/router";
+
+import {PedidoDto} from "../pedidos/pedidoDto";
+
+import {ProductoPedidoDto} from "../productos/productopedidodto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidosService {
   private url: string = 'http://localhost:8090/api/pedido'
-  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) {
 
   }
-  getPedido(id:number):Observable<Pedido> {
+
+  getPedido(id: number): Observable<Pedido> {
     return this.http.get<Pedido>(`${this.url}/${id}`).pipe()
+  }
+
+  postPedido(pedido: PedidoDto): Observable<any> {
+    return this.http.post<any>(this.url, pedido).pipe()
+
+  }
+
+  actualizarPedido(productos: ProductoPedidoDto, id: number): Observable<any> {
+    return this.http.put<any>(`${this.url}/${id}`, productos).pipe()
   }
 }
