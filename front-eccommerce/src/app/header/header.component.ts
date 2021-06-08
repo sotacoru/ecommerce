774b4f3fb6d0e0180817fbf6c24c28161eff1bed
antia.castro <antiacasga@gmail.com>
2021-06-08 17:53:35 +0200

    Cambio para que el carrito no aparezca cuando es admin

--- front-eccommerce/src/app/header/header.component.ts
+++ front-eccommerce/src/app/header/header.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { MenuItem } from 'primeng/api';
+import {MenuItem} from 'primeng/api';
 import {Categoria} from "../entity/categoria";
 import {ProductoService} from "../servicios/producto.service";
-import {tap} from "rxjs/operators";
-import { AuthUsuarioService } from '../servicios/auth-usuario-service';
-import { ModalUsuarioService } from '../modal-usuario/modal-usuario.service';
+import {AuthUsuarioService} from '../servicios/auth-usuario-service';
+import {ModalUsuarioService} from '../modal-usuario/modal-usuario.service';
 
 
 @Component({
@@ -18,34 +17,38 @@ export class HeaderComponent implements OnInit {
   itemsButton: MenuItem[] = [];
   items: MenuItem[] = [];
   subitems: MenuItem[] = [];
-  labelBoton: string = 'Log in';
-  labelBoolean: boolean = false;
   abierto: boolean = false;
   nombre: string = '';
 
   categorias: Categoria[] = [];
+
+
   constructor(private ps: ProductoService,
-    private authService: AuthUsuarioService,
-    private modalService: ModalUsuarioService) { }
+              private authService: AuthUsuarioService,
+              private modalService: ModalUsuarioService) {
+  }
 
   ngOnInit() {
+
     this.ps.getCategorias().subscribe(
       response => {
         response.forEach(
-          categoria=> {
-            this.subitems.push({label: categoria.nombrecategoria, routerLink: ['/productos/', categoria.nombrecategoria]})
+          categoria => {
+            this.subitems.push({
+              label: categoria.nombrecategoria,
+              routerLink: ['/productos/', categoria.nombrecategoria]
+            })
           }
         )
         this.categorias = response;
       }
-
     )
 
     this.items = [
       {
         label: 'Productos',
         items: [
-          { label: 'Todos los productos', routerLink: ['/productos'] },
+          {label: 'Todos los productos', routerLink: ['/productos']},
           {
             label: 'Categorias',
             items: this.subitems
@@ -55,25 +58,38 @@ export class HeaderComponent implements OnInit {
     ];
 
     this.itemsButton = [
-          {label: 'Información perfil', command: () => {this.abrirModal2()}},
-          {label: 'Administrar perfiles', routerLink:['/administrador/lista']},
-          {label: 'Cerrar sesión', command: () => {this.authService.logout()}}
+      {
+        label: 'Información perfil', command: () => {
+          this.abrirModal2()
+        }
+      },
+      {label: 'Administrar perfiles', routerLink: ['/administrador/lista']},
+      {
+        label: 'Cerrar sesión', command: () => {
+          this.authService.logout()
+        }
+      }
     ]
 
-    
-    if(this.nombre==''){
-      this.authService.getUsuario().subscribe( usuario =>{
-        this.nombre=usuario.nombre;
-      }
+
+    if (this.nombre == '') {
+      this.authService.getUsuario().subscribe(usuario => {
+          this.nombre = usuario.nombre;
+        }
       )
     }
   }
 
-  nombreUsuario(): String{
+  isCliente(): boolean {
+    return !this.authService.isAuthenticated() || this.authService.getPerfil() === 'CLIENTE';
+
+  }
+
+  nombreUsuario(): String {
     return this.nombre;
   }
 
-  isLogged(): boolean{
+  isLogged(): boolean {
     return this.authService.isAuthenticated();
   }
 
